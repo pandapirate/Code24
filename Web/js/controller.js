d526e98dab452c8c8098d9331028aa7bcfc7efb3
Kevin Huang <kevinchh88@gmail.com>
2014-07-20 18:28:46 -0700

    First working prototype

--- Web/js/controller.js
+++ Web/js/controller.js
@@ -1,5 +1,7 @@
 var equation = "";
 var numberClicked = false;
+var lastOp = "";
+var valueSaved = "";
 
 var operations = ["+", "&minus;", "&times;", "&divide;"];
 var used = [];
@@ -11,34 +13,96 @@ var clickNumber = function(numberID) {
     } else {
         equation += input[numberID];
         used.push(numberID);
+
+        if (lastOp === "") {
+            result = input[numberID];
+        } else {
+            switch (lastOp) {
+                case 0: result += input[numberID]; break;
+                case 1: result -= input[numberID]; break;
+                case 2: result *= input[numberID]; break;
+                case 3: result /= input[numberID]; break;
+            }
+            equation = "(" + equation + ")";
+            verifyResult();
+        }
+
         updateDisplay();
         numberClicked = true;
     }
+        console.log(equation + "=" + result);
+
 };
 
 var clickOp = function(opID) {
+
     if (numberClicked) {
         equation += operations[opID];
         updateDisplay();
         numberClicked = false;
+        lastOp = opID;
     } else {
         return;
     }
+        console.log(equation + "=" + result);
 };
+
+var saveValue = function() {
+
+    if (valueSaved === "") {
+        document.getElementById("savedValue").innerHTML = "<b>"+result+"</b>";
+        valueSaved = result;
+        result = 0;
+        equation = "";
+        lastOp = "";
+        numberClicked = false;
+    } else {
+        document.getElementById("savedValue").innerHTML = "<b>M</b>";
+        equation += valueSaved;
+
+        if (lastOp === "") {
+            result = valueSaved;
+        } else {
+            switch (lastOp) {
+                case 0: result += valueSaved; break;
+                case 1: result -= valueSaved; break;
+                case 2: result *= valueSaved; break;
+                case 3: result /= valueSaved; break;
+            }
+            equation = "(" + equation + ")";
+            verifyResult();
+        }
+        valueSaved = 0;
+        numberClicked = true;
+    }
+
+    updateDisplay();
+        console.log(equation + "=" + result);
+}
 
 var updateDisplay = function() {
     var ele = document.getElementById("solution");
     if (equation === "")
         ele.innerHTML = "";
     else
-        ele.innerHTML = equation + "=" + result;
+        ele.innerHTML = equation + " = " + result;
 };
+
+var verifyResult = function() {
+    if (used.length === 4 && result === 24) {
+        alert("You win");
+    }
+}
 
 var reset = function() {
     equation = "";
     result = 0;
     used = [];
     numberClicked = false;
+    lastOp = "";
+    valueSaved = "";
+    document.getElementById("savedValue").innerHTML = "<b>M</b>";
+
     updateDisplay();
 };
 
